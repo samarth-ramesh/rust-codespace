b0d76050dac4f86022bace687bd8350ba7433261
Samarth Ramesh <samarthr1@outlook.com>
2023-08-20 10:16:15 +0530

    WIP: rewrite

--- src/graph.rs
+++ src/graph.rs
@@ -1,93 +1,156 @@
-extern crate queues;
+pub mod graph {
+    use std::collections::hash_map::DefaultHasher;
+    use std::collections::HashMap;
+    use std::hash::{Hash, Hasher};
 
-use std::rc::Rc;
-use crate::graph::NodeType::{Group, Principal};
-use crate::graph::RelationType::{Belongs, Is};
-use queues::{IsQueue, Queue};
-#[derive(PartialEq)]
-pub enum RelationType {
-    Has,
-    Is,
-    Belongs,
-}
+    use queues::{IsQueue, Queue};
 
-pub struct Relation {
-    relation_type: RelationType,
-    relation: Box<Node>,
-}
+    use crate::graph::graph::Status::{DuplicateRelation, NotFound, Ok};
 
-impl Relation {
-    pub fn new(relation_type: RelationType, relation: Box<Node>) -> Self {
-        Self { relation_type, relation }
-    }
-}
-#[derive(Clone)]
-pub enum NodeType {
-    Principal,
-    Target,
-    Group,
-}
+    extern crate queues;
 
-pub struct Node {
-    node_type: NodeType,
-    relations: Vec<Relation>,
-    value: String,
-}
-
-pub struct Graph {
-    v: Vec<Box<Node>>,
-}
-
-impl Graph {
-    pub fn new() -> Self {
-        Self { v: vec![] }
+    pub enum Status {
+        Ok,
+        NotFound,
+        DuplicateRelation,
     }
 
-    pub fn add_principal(&mut self, value: String) {
-        let r = Box::new(Node::new(Principal, value));
-        self.v.push(r)
+    #[derive(Hash, Clone, Copy)]
+    pub enum RelationType {
+        Is,
+        Belongs,
+        Has,
+        Implies,
     }
 
-    pub fn print_principals(&self) {
-        for i in self.v.as_slice() {
-            println!("{}", i.value)
-        }
+    #[derive(Hash, Eq, PartialEq)]
+    pub enum NodeType {
+        Principal,
+        // has indegree == 0
+        Permission,
+        // has outdegree == 0
+        Group, // can have any indegree or outdegree
     }
 
-    pub fn create_group(&mut self, value: String, idx: usize) {
-        let r = Box::from(Node::new(Group, value));
-        self.v[idx].relations.push(Relation::new(Belongs, r))
+    // used for adj. list calculations
+    #[derive(Hash, Eq, PartialEq)]
+    pub struct Node {
+        uid: String,
+        node_type: NodeType,
     }
 
-    pub fn get_principal(&self, name: String) -> (Option<usize>) {
-        for i in 0..self.v.len() {
-            if self.v[i].value == name {
-                return Option::from(i);
+    pub struct Graph {
+        adj_matrix: Box<HashMap<Node, HashMap<Node, RelationType>>>,
+    }
+
+    impl Graph {
+        pub fn new() -> Self {
+            Self {
+                adj_matrix: Box::from(HashMap::new())
             }
         }
-        return None
-    }
 
-    pub fn does_relation_exist(&self, start: usize, dest: String, dest_relation_type: RelationType) -> bool {
-        let mut q: Queue<&Node> = Queue::new();
-        q.add(self.v[start].as_ref()).expect("could not add");
-        while q.size() > 0 {
-            let cur = q.remove().expect("could not pop");
-            for relation in cur.relations.as_slice() {
-                if relation.relation_type == dest_relation_type && relation.relation.value == dest {
-                    return  true;
-                } else {
-                    q.add(relation.relation.as_ref()).expect("could not add in BFS");
+        pub fn get_hash(node: *const Node) -> u64 {
+            let mut hasher = DefaultHasher::new();
+            node.hash(&mut hasher);
+            return hasher.finish();
+        }
+
+        pub fn insert_generic(&mut self, uid: String, node_type: NodeType) {
+            let n = Node::new(uid, node_type);
+            self.adj_matrix.insert(n, HashMap::new());
+        }
+
+        pub fn add_link(&mut self, uid1: String, uid2: String, node_type1: NodeType, node_type2: NodeType, relation_type: RelationType) -> Status {
+            let n1 = Node::new(uid1, node_type1);
+            let n2 = Node::new(uid2, node_type2);
+
+            {
+                // checking phase
+                let r1 = self.adj_matrix.get(&n1);
+                if r1.is_none() {
+                    return NotFound;
+                }
+                let r1 = r1.unwrap();
+
+                // sanity check. Should never usually be the case
+                if r1.contains_key(&n2){
+                    return DuplicateRelation;
+                }
+
+                let does_dest_exist = self.adj_matrix.contains_key(&n2);
+                if !does_dest_exist {
+                    return NotFound;
                 }
             }
+
+
+            {
+                // time to yeet stuff in
+                let r1 = self.adj_matrix.get_mut(&n1).unwrap();
+                r1.insert(n2, relation_type);
+            }
+
+            return Ok;
         }
-        return false;
+        pub fn check_link(&self, uid1: String, uid2: String, node_type1: NodeType, node_type2: NodeType) -> Result<bool, Status> {
+            let n1 = Node::new(uid1, node_type1);
+            let n2 = Node::new(uid2, node_type2);
+            let h2 = Self::get_hash(&n2);
+
+            // checking phase
+            let r1 = self.adj_matrix.get(&n1);
+            let does_dest_exist = self.adj_matrix.contains_key(&n2);
+            if r1.is_none() || !does_dest_exist {
+                return Err(NotFound);
+            }
+
+            let mut q: Queue<&Node> = Queue::new();
+            let mut visited: HashMap<u64, bool> = HashMap::new();
+            q.add(&n1).unwrap();
+
+            while q.size() > 0 {
+                let cur = q.remove().unwrap();
+                let cur_hash = Self::get_hash(cur);
+                if visited.contains_key(&cur_hash) {
+                    continue;
+                } else {
+                    visited.insert(cur_hash, true);
+                }
+
+                if cur_hash.eq(&h2) {
+                    return Result::Ok(true);
+                }
+
+                let cur_children = self.adj_matrix.get(cur).unwrap();
+                cur_children.iter().for_each(|(key, _)| {
+                    q.add(key).unwrap();
+                })
+            }
+
+            return Result::Ok(false);
+        }
+    }
+
+    impl Node {
+        pub fn new(uid: String, node_type: NodeType) -> Self {
+            Self { uid, node_type }
+        }
     }
 }
 
-impl Node {
-    pub fn new(node_type: NodeType, value: String) -> Self {
-        Self { node_type, relations: vec![], value }
+#[cfg(test)]
+mod tests {
+    use crate::graph::graph::Graph;
+    use crate::graph::graph::NodeType::{Permission, Principal};
+    use crate::graph::graph::RelationType::Has;
+
+    #[test]
+    fn test_create_link() {
+        let mut g = Graph::new();
+        g.insert_generic(String::from("1"), Principal);
+        g.insert_generic(String::from("2"), Permission);
+        g.add_link(String::from("1"), String::from("2"), Principal, Permission, Has);
+        assert!(g.check_link(String::from("1"), String::from("2"), Principal, Permission).is_ok_and(|x| {x}));
     }
 }
-
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
-use crate::graph::RelationType::{Belongs, Is};
-
 mod graph;
 
+use graph::graph as g;
+
 fn main() {
-    let mut g = graph::Graph::new();
-    let s = String::from("hi");
-    g.add_principal(s.clone());
-    g.print_principals();
-    let item_idx = g.get_principal(s.clone()).expect("Error");
-    g.create_group(s, item_idx);
+    println!("Hello, world!");
 }
